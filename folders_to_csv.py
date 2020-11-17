d15a1e96470b9f9348f20fb5bc4a2fcb9a6f03cf
GuillaumeClementPolytech <guillaume.clement@etu.umontpellier.fr>
2020-11-17 10:56:20 +0100

    template file

---
+++ folders_to_csv.py
@@ -0,0 +1,18 @@
+"""module Docstring"""
+
+if __name__ == "__main__":
+    print("Running as main") 
+    
+class UneClasse:
+    """Class Docstring"""
+    
+    def return_self(self):
+        """
+        method docstring
+        """
+        return self
+    
+def return_5():
+    """function docstring"""
+    return 5
+

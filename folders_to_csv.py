ab62e6f65637820de301cbe0545753da6db26b3e
GuillaumeClementPolytech <guillaume.clement@etu.umontpellier.fr>
2020-11-17 11:30:37 +0100

    start

--- folders_to_csv.py
+++ folders_to_csv.py
@@ -1,18 +1,40 @@
-"""module Docstring"""
+"""
+Create a cvs file using a folder of mails
+folder
+|_a-name
+| |_folder_name
+| | |_mail
+| | |_mail
+| |_folder_name
+|   |_mail
+|   |_mail
+|   |_mail
+|_...
+|
+...
+"""
+import csv
+
+
+def convert_to_csv(folder_path: str):
+    """
+    Take a path to a folder, and return an array
+    """
+    raise NotImplementedError
+
+
+def write_to_csv(folder_path: str, array):
+    """
+    Take an array, write a csv to the output path
+    """
+    raise NotImplementedError
+
 
 if __name__ == "__main__":
-    print("Running as main") 
+    print("Running as main")
+    MAILS_FOLDER_PATH = "../maildir"
+    OUTPUT_CSV_PATH = "../mails.csv"
     
-class UneClasse:
-    """Class Docstring"""
-    
-    def return_self(self):
-        """
-        method docstring
-        """
-        return self
-    
-def return_5():
-    """function docstring"""
-    return 5
+    ARRAY = convert_to_csv(MAILS_FOLDER_PATH)
+    write_to_csv(OUTPUT_CSV_PATH, ARRAY)
 

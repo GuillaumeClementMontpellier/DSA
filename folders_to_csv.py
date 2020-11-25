720b4fb212e5ce118ad7e8f46d984512f913145c
GuillaumeClementPolytech <guillaume.clement@etu.umontpellier.fr>
2020-11-25 12:27:51 +0100

    voila

--- folders_to_csv.py
+++ folders_to_csv.py
@@ -18,7 +18,7 @@ from typing import List
 import os
 
 
-def parse_file(file_path: str, folder_name: str) -> List[str]:
+def parse_file(file_path: str, folder_name: str) -> List[List[str] ]:
     """
     Parse a mail file into a List[str]
     avec envoyeur, destinataire, sujet, Date, folderName, metadata, content
@@ -26,33 +26,16 @@ def parse_file(file_path: str, folder_name: str) -> List[str]:
     # print(file_path)
     with open(file_path, mode="r", newline="") as file:
         sender = 0
-        recipients = 0
-        subject = 0
-        date = 0
-        metadata = []
-        content = []
-        metadata_ended = False
+        recipients = []
+        
         for line in file:
-            if ":" in line:
-                data = line.split(":")
-                if (data[0] == "From" and sender == 0):
-                    sender = data[1].strip()
-                elif (data[0] == "To" and recipients == 0):
-                    recipients = list(map(str.strip, data[1].split(",")))
-                elif (data[0] == "Date" and date == 0):
-                    date = data[1].strip()
-                elif (data[0] == "Subject" and subject == 0):
-                    subject = data[1].strip()
-                else:
-                    if not metadata_ended:
-                        metadata.append(line.strip())
-                    # else:
-                    #     content.append(line.strip())
-            else:
-                metadata_ended = True
-                # content.append(line.strip())
-                
-        return [sender, recipients, subject, date, folder_name, metadata, " ".join(content)]
+            data = line.split(":")
+            if (data[0] == "From" and sender == 0):
+                sender = data[1].strip()
+            elif (data[0] == "To" and recipients == []):
+                recipients += list(map(str.strip, data[1].split(",")))
+               
+        return map(lambda a: [sender, a] ,recipients)
 
 
 def convert_to_csv(folder_path: str, writer) -> List[List[str]]:
@@ -61,18 +44,19 @@ def convert_to_csv(folder_path: str, writer) -> List[List[str]]:
     If folder, call itself on every child and concatenate the arrays
     If file, call and return [parse_file]
     """
+    base_path = folder_path
     def convert_to_csv_rec(folder_path: str, caller_folder: str, writer)-> List[List[str]]:
+        if caller_folder == base_path:
+            print(folder_path)
         if os.path.isdir(folder_path):
             for folder in os.listdir(folder_path):
                 convert_to_csv_rec(os.path.join(folder_path, folder), folder_path, writer)
         elif os.path.isfile(folder_path):
-            writer.writerow(parse_file(folder_path, caller_folder))
+            for row in parse_file(folder_path, caller_folder):
+                writer.writerow(row)
         else:
             raise FileNotFoundError("Data folder not found in: " + folder_path)
-    writer.writerow(["sender", "recipients",
-                     "subject", "date",
-                     "folder_name", "metadata",
-                     "content"])
+    writer.writerow(["sender", "recipient"])
     convert_to_csv_rec(folder_path, "base", writer)
 
 
